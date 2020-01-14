21046bcfa53ef9b65dc09d0f4d9349149b85c642
Tom <itagaki.tomoya@eversense.co.jp>
2020-01-14 19:33:40 +0900

    タブ動く

--- Pods/TabPageViewController/Sources/TabPageOption.swift
+++ Pods/TabPageViewController/Sources/TabPageOption.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
+public enum HidesTopContentsOnSwipeType {
+    case none
+    case tabBar
+    case navigationBar
+    case all
+}
+
 public struct TabPageOption {
-
+    
     public init() {}
-
+    
     public var fontSize = UIFont.systemFontSize
     public var currentColor = UIColor(red: 105/255, green: 182/255, blue: 245/255, alpha: 1.0)
     public var defaultColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0)
@@ -22,15 +29,15 @@ public struct TabPageOption {
     public var tabBackgroundColor: UIColor = .white
     public var pageBackgoundColor: UIColor = UIColor.white
     public var isTranslucent: Bool = true
-    public var hidesTabBarOnSwipe: Bool = false
-
+    public var hidesTopViewOnSwipeType: HidesTopContentsOnSwipeType = .none
+    
     internal var tabBarAlpha: CGFloat {
         return isTranslucent ? 0.95 : 1.0
     }
     internal var tabBackgroundImage: UIImage {
         return convertImage()
     }
-
+    
     fileprivate func convertImage() -> UIImage {
         let rect : CGRect = CGRect(x: 0, y: 0, width: 1, height: 1)
         UIGraphicsBeginImageContext(rect.size)
---
+++ tab_practice_line/InfiniteTabPageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InfiniteTabPageViewController.swift
+//  tab_practice_line
+//
+//  Created by eversense on 2020/01/14.
+//  Copyright © 2020 eversense. All rights reserved.
+//
+
+import UIKit
+import TabPageViewController
+
+class InfiniteTabPageViewController: TabPageViewController {
+    
+    override init() {
+        super.init()
+        let vc1 = UIViewController()
+        vc1.view.backgroundColor = UIColor(red: 251/255, green: 252/255, blue: 149/255, alpha: 1.0)
+        let vc2 = UIViewController()
+        vc2.view.backgroundColor = UIColor(red: 252/255, green: 150/255, blue: 149/255, alpha: 1.0)
+        let vc3 = UIViewController()
+        vc3.view.backgroundColor = UIColor(red: 149/255, green: 218/255, blue: 252/255, alpha: 1.0)
+        let vc4 = UIViewController()
+        vc4.view.backgroundColor = UIColor(red: 149/255, green: 252/255, blue: 197/255, alpha: 1.0)
+        let vc5 = UIViewController()
+        vc5.view.backgroundColor = UIColor(red: 252/255, green: 182/255, blue: 106/255, alpha: 1.0)
+        let vc6 = UIViewController()
+        vc6.view.backgroundColor = UIColor(red: 149/255, green: 182/255, blue: 106/255, alpha: 1.0)
+        
+        tabItems = [(vc1, "あるある"), (vc2, "やっちまった！"), (vc3, "つわり"), (vc4, "育児あるある"), (vc5, "離乳食"), (vc6, "妊娠")]
+        isInfinity = false
+        
+        option.tabMargin = 10.0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
